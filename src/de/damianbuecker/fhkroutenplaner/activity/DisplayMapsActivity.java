7a2f34b7451f756672219459d090e1c11a8d037d
dbuecker <dbuecker@fh-koeln.de>
2014-10-11 17:23:06 +0200

    WebView in DisplayMapsActivity

--- src/de/damianbuecker/fhkroutenplaner/activity/DisplayMapsActivity.java
+++ src/de/damianbuecker/fhkroutenplaner/activity/DisplayMapsActivity.java
@@ -7,34 +7,52 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+/**
+ * The Class DisplayMapsActivity.
+ */
 @ContentView(R.layout.display_maps_activity)
 public class DisplayMapsActivity extends ModifiedViewActivityImpl {
 	
+	/** The list items. */
 	private String[] listItems = {"Test"};
 	
+	/** The m drawer layout. */
 	@InjectView(R.id.drawer_layout)
 	private DrawerLayout mDrawerLayout;
 
+	/** The m drawer list. */
 	@InjectView(R.id.left_drawer)
 	private ListView mDrawerList;
 	
+	/** The btn toggle drawer. */
 	@InjectView(R.id.btntoggledrawer)
 	private Button btnToggleDrawer;
 	
+	/** The m list view drawer. */
 	@InjectView(R.id.left_drawer)
 	private ListView mListViewDrawer;
 	
+	/** The btn left. */
 	@InjectView(R.id.btnleft)
 	private Button btnLeft;
 	
+	/** The btn right. */
 	@InjectView(R.id.btnright)
 	private Button btnRight;
-
+	
+	/** The m web view. */
+	@InjectView(R.id.webView)
+	private WebView mWebView;
+	
+	/* (non-Javadoc)
+	 * @see roboguice.activity.RoboActivity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -45,17 +63,13 @@ public class DisplayMapsActivity extends ModifiedViewActivityImpl {
 		this.btnToggleDrawer.setOnClickListener(new ButtonDrawerToggleListener());
 		this.btnLeft.setOnClickListener(new ButtonLeftRightListener());
 		this.btnRight.setOnClickListener(new ButtonLeftRightListener());
-		
 	}
 	
-	private class DrawerItemClickListener implements ListView.OnItemClickListener {
-
-		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			selectItem(position);
-		}
-	}
-	
+	/**
+	 * Select item.
+	 *
+	 * @param position the position
+	 */
 	private void selectItem(int position) {
 		Intent intent = new Intent(this, NavigationActivity.class);
 		startActivity(intent);
@@ -64,8 +78,44 @@ public class DisplayMapsActivity extends ModifiedViewActivityImpl {
 		 */
 	}
 	
+	/**
+	 * The listener interface for receiving drawerItemClick events.
+	 * The class that is interested in processing a drawerItemClick
+	 * event implements this interface, and the object created
+	 * with that class is registered with a component using the
+	 * component's <code>addDrawerItemClickListener<code> method. When
+	 * the drawerItemClick event occurs, that object's appropriate
+	 * method is invoked.
+	 *
+	 * @see DrawerItemClickEvent
+	 */
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+		/* (non-Javadoc)
+		 * @see android.widget.AdapterView.OnItemClickListener#onItemClick(android.widget.AdapterView, android.view.View, int, long)
+		 */
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			selectItem(position);
+		}
+	}
+	
+	/**
+	 * The listener interface for receiving buttonDrawerToggle events.
+	 * The class that is interested in processing a buttonDrawerToggle
+	 * event implements this interface, and the object created
+	 * with that class is registered with a component using the
+	 * component's <code>addButtonDrawerToggleListener<code> method. When
+	 * the buttonDrawerToggle event occurs, that object's appropriate
+	 * method is invoked.
+	 *
+	 * @see ButtonDrawerToggleEvent
+	 */
 	private class ButtonDrawerToggleListener implements View.OnClickListener {
 
+		/* (non-Javadoc)
+		 * @see android.view.View.OnClickListener#onClick(android.view.View)
+		 */
 		@Override
 		public void onClick(View v) {
 			if(DisplayMapsActivity.this.mDrawerLayout.isDrawerOpen(DisplayMapsActivity.this.mListViewDrawer)) {
@@ -76,8 +126,22 @@ public class DisplayMapsActivity extends ModifiedViewActivityImpl {
 		}
 	}
 	
+	/**
+	 * The listener interface for receiving buttonLeftRight events.
+	 * The class that is interested in processing a buttonLeftRight
+	 * event implements this interface, and the object created
+	 * with that class is registered with a component using the
+	 * component's <code>addButtonLeftRightListener<code> method. When
+	 * the buttonLeftRight event occurs, that object's appropriate
+	 * method is invoked.
+	 *
+	 * @see ButtonLeftRightEvent
+	 */
 	private class ButtonLeftRightListener implements View.OnClickListener {
 
+		/* (non-Javadoc)
+		 * @see android.view.View.OnClickListener#onClick(android.view.View)
+		 */
 		@Override
 		public void onClick(View v) {
 			if(v.getId() == R.id.btnleft) {
@@ -92,6 +156,5 @@ public class DisplayMapsActivity extends ModifiedViewActivityImpl {
 				DisplayMapsActivity.this.btnLeft.setVisibility(View.VISIBLE);
 			}
 		}
-		
 	}
 }
